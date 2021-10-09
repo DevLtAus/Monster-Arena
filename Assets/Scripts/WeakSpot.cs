fced9d7c1334eda4114de0ec6f658230061426d0
Elliot <elliot.holt@students.mq.edu.au>
2021-10-09 16:06:09 +1100

    Weak Spot health bars done

--- Assets/Scripts/WeakSpot.cs
+++ Assets/Scripts/WeakSpot.cs
@@ -1,15 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class WeakSpot : MonoBehaviour
 {
     //(Elliot) health is public variable with value of 1 by default
     public int health = 1;
+    Slider wSlider;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        wSlider = GetComponentInChildren<Slider>();
+        wSlider.maxValue = health;
+        wSlider.value = health;
     }
 
     // Update is called once per frame
@@ -27,5 +32,6 @@ public class WeakSpot : MonoBehaviour
             health = 0;
             gameObject.SetActive(false);
         }
+        wSlider.value = health;
     }
 }
