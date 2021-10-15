bc8c194ea1a17fabb759829f0112f9fb86a1ec07
Lucas Turnbull <link.turnbull@gmail.com>
2021-10-15 19:29:25 +1100

    Further improved the eye attack
    Aside from having it talk to it's danger zone and making it shoot at the player, it's finished

--- Assets/Scripts/EyeAttack.cs
+++ Assets/Scripts/EyeAttack.cs
@@ -11,8 +11,24 @@ public class EyeAttack : MonoBehaviour
     private float targetDistance = 0;
     public Vector3 hitPos;
     private ContactFilter2D filter;
-    public RaycastHit2D[] hits;
+
+    // (Lucas) Raycast for aiming.
+    private RaycastHit2D[] hits;
     private int hitCount;
+
+    // (Lucas) Timer for attack delay.
+    private float attackDelayTimer = 0;
+    public float attackDelay;
+    public bool attacking = false;
+
+    // (Lucas) Timer for attack cooldown.
+    private float cooldownTimer = 0;
+    public float cooldown;
+    public bool coolingDown = false;
+
+    // (Lucas) Are we allowed to attack?
+    public bool attackAllowed = false;
+
     //public LayerMask mask;
 
     // Start is called before the first frame update
@@ -23,25 +39,74 @@ public class EyeAttack : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        // (Lucas) Where to aim and how far away the player is.
         targetDistance = Vector3.Distance(target.position, transform.position);
         Vector3 targetDirection = target.position - transform.position;
         float singleStep = speed * Time.deltaTime;
         Vector3 newDirection = Vector3.RotateTowards(transform.forward, targetDirection, singleStep, 0.0f);
-        //RaycastHit2D hit = 
         hits = new RaycastHit2D[20];
         hitCount = Physics2D.Raycast(transform.position, newDirection, filter, hits);
         RaycastHit2D hit = hits[1];
-        if (hit.collider != null)
-        {
+        if (hit.collider != null) {
             hitPos = hit.transform.position;
             //distance = Mathf.Abs(hit.point.y - transform.position.y);
             Vector2 myPos = new Vector2(transform.position.x, transform.position.y);
             distance = Vector2.Distance(hit.point, myPos);
+            if (hit.transform.gameObject.tag == "Player" && !coolingDown && attackAllowed && !attacking) {
+                attacking = true;
+                attackDelayTimer = attackDelay;
+            }
         }
         Debug.DrawRay(transform.position, newDirection * targetDistance, Color.green);
         Debug.DrawRay(transform.position, newDirection * distance, Color.red);
-        transform.rotation = Quaternion.LookRotation(newDirection);
+        //transform.rotation = Quaternion.LookRotation(newDirection);
+
+        switch(attacking) {
+            case false:
+                transform.rotation = Quaternion.LookRotation(newDirection);
+                switch(coolingDown) {
+                    case true:
+                        if (cooldownTimer > 0) {
+                            cooldownTimer -= 1;
+                        }
+                        else {
+                            coolingDown = false;
+                        }
+                        break;
+                    case false:
+                        break;
+                }
+                break;
+            case true:
+                switch(attackAllowed) {
+                    case true:
+                        //Attack();
+                        if (attackDelayTimer > 0) {
+                            attackDelayTimer -= 1;
+                        }
+                        else {
+                            Attack();
+                        }
+                        break;
+                    case false:
+                        break;
+                }
+                break;
+        }
+    }
+
+    private void Attack()
+    {
+        Debug.Log("Attacking the player");
+        attacking = false;
+        Cooldown();
+    }
+
+    private void Cooldown()
+    {
+        cooldownTimer = cooldown;
+        coolingDown = true;
     }
 }
