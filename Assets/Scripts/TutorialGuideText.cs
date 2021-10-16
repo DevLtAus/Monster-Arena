51a9acb7e0739fd3c1b1df1c13ff1a9022d5d07a
Lucas Turnbull <link.turnbull@gmail.com>
2021-10-16 15:58:10 +1100

    Got rid of pointless comments
    Went through a few options on how to fade the text in and out, deleted the commented out code from stuff I considered previously.

--- Assets/Scripts/TutorialGuideText.cs
+++ Assets/Scripts/TutorialGuideText.cs
@@ -6,9 +6,6 @@ using UnityEngine.UI;
 public class TutorialGuideText : MonoBehaviour
 {
     public float fadeTime;
-    //private float fadeTimer;
-
-    //public BoxCollider2D showZone;
     public Text textToShow;
     public bool showing;
 
@@ -17,7 +14,6 @@ public class TutorialGuideText : MonoBehaviour
         // (Lucas) Show the text.
         if (col.gameObject.tag == "Player") {
             //Debug.Log("Showing text");
-            //StartCoroutine(FadeTextIn());
             showing = true;
         }
     }
@@ -27,34 +23,10 @@ public class TutorialGuideText : MonoBehaviour
         // (Lucas) Hide the text.
         if (col.gameObject.tag == "Player") {
             //Debug.Log("Hiding text");
-            //StartCoroutine(FadeTextOut());
             showing = false;
         }
     }
 
-    /*public IEnumerator FadeTextIn()
-    {
-        while (textToShow.color.a < 1)
-        {
-            Debug.Log("Fading in...");
-            //textToShow.color = new Color(textToShow.color.r, textToShow.color.g, textToShow.color.b, textToShow.color.a + (Time.deltaTime / fadeTime));
-            textToShow.CrossFadeAlpha(1, fadeTime, false);
-            yield return null;
-        }
-        //yield return null;
-    }*/
-
-    /*public IEnumerator FadeTextOut()
-    {
-        while (textToShow.color.a < 0)
-        {
-            Debug.Log("Fading out...");
-            //textToShow.color = new Color(textToShow.color.r, textToShow.color.g, textToShow.color.b, textToShow.color.a - (Time.deltaTime / fadeTime));
-            textToShow.CrossFadeAlpha(0, fadeTime, false);
-            yield return null;
-        }
-        //yield return null;
-    }*/
     // Start is called before the first frame update
     void Start()
     {
