34b106ae96b65dd0acf833cec85747240d3c9922
Lucas Turnbull <link.turnbull@gmail.com>
2021-11-02 17:26:48 +1100

    Added cloud fade in
    Now just needs to do damage to the player if they spend too long in the cloud and the boss attacks will have all been created

--- Assets/Scripts/Cloud.cs
+++ Assets/Scripts/Cloud.cs
@@ -4,15 +4,66 @@ using UnityEngine;
 
 public class Cloud : MonoBehaviour
 {
+    //private int levels;
+    private int curLevel = 0;
+    public CloudLevel[] cloudLevels;
+    private bool fadedIn = false;
+    private bool damaging = false;
+    private PolygonCollider2D poly;
     // Start is called before the first frame update
     void Start()
     {
-        
+        poly = gameObject.GetComponent<PolygonCollider2D>();
+        poly.enabled = false;
+    }
+
+    void Awake()
+    {
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        switch (fadedIn) {
+            case true:
+                switch (damaging) {
+                    case false:
+                        poly.enabled = true;
+                        break;
+                    case true:
+                        break;
+                }
+                break;
+            case false:
+                switch (cloudLevels[curLevel].faded) {
+                    case true:
+                        if (curLevel < cloudLevels.Length-1) {
+                            curLevel += 1;
+                        }
+                        else {
+                            fadedIn = true;
+                        }
+                        break;
+                    case false:
+                        switch (cloudLevels[curLevel].fading) {
+                            case true:
+                                break;
+                            case false:
+                                StartCoroutine(cloudLevels[curLevel].Fade());
+                                break;
+                        }
+                        //StartCoroutine(cloudLevels[curLevel].Fade());
+                        break;
+                }
+                break;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player") {
+            Debug.Log("Player is in danger");
+        }
     }
 }
--- Assets/Scripts/CloudLevel.cs
+++ Assets/Scripts/CloudLevel.cs
@@ -4,15 +4,52 @@ using UnityEngine;
 
 public class CloudLevel : MonoBehaviour
 {
+    private SpriteRenderer[] sRend;
+    public float fadeSpeed = 0.05f;
+    public bool fading = false;
+    public bool faded = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        sRend = GetComponentsInChildren<SpriteRenderer>();
+        Color temp = sRend[0].color;
+        temp.a = 0;
+        foreach (SpriteRenderer sr in sRend)
+        {
+            sr.color = temp;
+        }
+    }
+
+    void Awake()
+    {
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public IEnumerator Fade()
+    {
+        fading = true;
+        float curAlpha = sRend[0].color.a;
+        Color temp = sRend[0].color;
+
+        while (sRend[0].color.a < 1)
+        {
+            curAlpha += 0.01f;
+            temp.a = curAlpha;
+            foreach (SpriteRenderer sr in sRend)
+            {
+                sr.color = temp;
+            }
+            yield return new WaitForSeconds(fadeSpeed);
+        }
+        //Destroy(gameObject);
+        faded = true;
+        fading = false;
     }
 }
